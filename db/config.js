ab6fa49681daf78889433af755b1db00bf8e55f8
Sudesh Jadhav <sud.jad3698@gmail.com>
2024-09-02 18:52:51 +0530

    quich changes

--- db/config.js
+++ db/config.js
@@ -1,28 +1,28 @@
 // config.js
 module.exports = {
     development: {
-     "username": process.env.DB_USER,
-     "password": process.env.DB_PASSWORD,
-     "database": process.env.DB_NAME,
-     "host": process.env.DB_HOST,
-     "dialect": process.env.DB_DIALECT,
-     "port": process.env.DB_PORT
+     "username": process.env.DEV_DB_USER,
+     "password": process.env.DEV_DB_PASSWORD,
+     "database": process.env.DEV_DB_NAME,
+     "host": process.env.DEV_DB_HOST,
+     "dialect": process.env.DEV_DB_DIALECT,
+     "port": process.env.DEV_DB_PORT
     },
     test: {
-      "username": process.env.DB_USER,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
-      "host": process.env.DB_HOST,
-      "dialect": process.env.DB_DIALECT,
-      "port": process.env.DB_PORT
+      "username": process.env.TEST_DB_USER,
+      "password": process.env.TEST_DB_PASSWORD,
+      "database": process.env.TEST_DB_NAME,
+      "host": process.env.TEST_DB_HOST,
+      "dialect": process.env.TEST_DB_DIALECT,
+      "port": process.env.TEST_DB_PORT
     },
     production: {
-      "username": process.env.DB_USER,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
-      "host": process.env.DB_HOST,
-      "dialect": process.env.DB_DIALECT,
-      "port": process.env.DB_PORT
+      "username": process.env.PROD_DB_USER,
+      "password": process.env.PROD_DB_PASSWORD,
+      "database": process.env.PROD_DB_NAME,
+      "host": process.env.PROD_DB_HOST,
+      "dialect": process.env.PROD_DB_DIALECT,
+      "port": process.env.PROD_DB_PORT
     }
   };
   
