e8d293594ce6c030bedc6a4b5a19392d3510e2b6
Sudesh Jadhav <sud.jad3698@gmail.com>
2024-09-02 18:48:33 +0530

    quick changes

--- db/config.js
+++ db/config.js
@@ -1,28 +1,28 @@
 // config.js
 module.exports = {
     development: {
-     "username": "postgres",
-     "password": "root",
-     "database": "dashboard",
-     "host": "localhost",
-     "dialect": "postgres",
-     "port": 5432
+     "username": process.env.DB_USER,
+     "password": process.env.DB_PASSWORD,
+     "database": process.env.DB_NAME,
+     "host": process.env.DB_HOST,
+     "dialect": process.env.DB_DIALECT,
+     "port": process.env.DB_PORT
     },
     test: {
-      "username": "postgres",
-      "password": "root",
-      "database": "dashboard",
-      "host": "localhost",
-      "dialect": "postgres",
-      "port": 5432
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
+      "dialect": process.env.DB_DIALECT,
+      "port": process.env.DB_PORT
     },
     production: {
-      "username": "postgres",
-      "password": "root",
-      "database": "dashboard",
-      "host": "localhost",
-      "dialect": "postgres",
-      "port": 5432
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
+      "dialect": process.env.DB_DIALECT,
+      "port": process.env.DB_PORT
     }
   };
   
